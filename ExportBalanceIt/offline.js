0c883788fe0eb3684bf5517a8f0d62e30d54b727
C24-S60348 <afwanhaziq987@gmail.com>
2022-08-02 11:14:39 +0800

    Balance IT Update FIRST Export!!

---
+++ ExportBalanceIt/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1659409913,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/shape-sheet0.png",
+		"images/shape-sheet1.png",
+		"images/faces-sheet0.png",
+		"images/faces-sheet1.png",
+		"images/tile.png",
+		"images/garisan.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
