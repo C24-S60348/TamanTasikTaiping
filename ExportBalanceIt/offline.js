eecae3ad70275d0b0dd49a018fbbcd9e106a3995
C24-S60348 <afwanhaziq987@gmail.com>
2022-08-04 00:56:19 +0800

    Balance It level 1 siap separuh

--- ExportBalanceIt/offline.js
+++ ExportBalanceIt/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1659410448,
+	"version": 1659545685,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -12,11 +12,16 @@
 		"images/faces-sheet1.png",
 		"images/tile.png",
 		"images/garisan.png",
+		"images/tbputih.png",
+		"images/cancelput-sheet0.png",
+		"images/taklehtekan-sheet0.png",
+		"images/garisan2.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"level1.json"
 	]
 }
